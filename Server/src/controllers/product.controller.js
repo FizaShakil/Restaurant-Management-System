6406359307e7a6e8e4101afe7b66db6422b38eec
FizaShakil <fizashakil25@gmail.com>
2025-05-29 23:26:13 +0500

    Added up the product router and controllers

---
+++ Server/src/controllers/product.controller.js
@@ -0,0 +1,113 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import connection from "../db/connection.js";
+
+const addProductDeatils = asyncHandler(async(req,res)=>{
+    const product = req.body
+    const addedProduct = await connection.execute(
+        "INSERT into product (name, categoryId, description, price, status) VALUES(?,?,?,?,'true')",
+        [product.name, product.categoryId,product.description,product.price]
+    )
+
+    if(!addedProduct){
+        throw new ApiError(401, "Incomplete details")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(200, product, "Product Deatils added successfully")
+    )
+})
+
+const getProductDetails = asyncHandler(async(req,res)=>{
+    const [getDetails]= await connection.execute(
+        "SELECT p.id, p.name, p.description, p.price, p.status, c.id as categoryId, c.name as categoryName from product as p INNER JOIN category as c WHERE p.categoryId = c.id"
+    )
+    if(!getDetails){
+        throw new ApiError(500, "Error fetching product details")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(200, getDetails, "Product Details fetched successfully")
+    )
+})
+
+const getByCategoryId = asyncHandler(async(req,res)=>{
+    const id = req.params.id
+    const [getByCatId] = await connection.execute(
+        "SELECT id, name FROM product WHERE categoryId=? and status='true' ",
+        [id]
+    )
+    if(!getByCatId){
+        throw new ApiError(500, "Error in fetching product details by category ID")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, getByCatId, "Products by category ID fetched successfully")
+    )
+})
+
+const getById = asyncHandler(async(req,res)=>{
+    const id = req.params.id
+    const [getById] = await connection.execute(
+        "SELECT id, name, description, price FROM product WHERE id=? ",
+        [id]
+    )
+    if(!getById){
+        throw new ApiError(500, "Error in fetching product details by product ID")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, getById, "Products by product ID fetched successfully")
+    )
+})
+
+const updateProductDetails = asyncHandler(async(req,res)=>{
+    const product = req.body
+    const [updateProduct] = await connection.execute(
+        "UPDATE product SET name=?, categoryId=?, description=?, price=? WHERE id=? ",
+        [product.name,product.categoryId, product.description, product.price,product.id]
+    )
+    if(updateProduct.affectedRows == 0){
+        throw new ApiError(400, "Product ID doesnot exist")
+    }
+    
+    return res.status(200)
+    .json(
+        new ApiResponse(200, product, "Product Details updated successfully! ")
+    )
+})
+
+const deleteProduct = asyncHandler(async(req,res)=>{
+    const id = req.params.id;
+    const [deleteProduct] = await connection.execute(
+        "DELETE from product WHERE id=?",
+        [id]
+    )
+   if(deleteProduct.affectedRows == 0){
+    throw new ApiError(400, "Product ID not found")
+   }
+
+   return res.status(200)
+   .json(
+    new ApiResponse(200, deleteProduct, "Product Deleted Successfully")
+   )
+})
+
+const updateStatus = asyncHandler(async(req,res)=>{
+    const user = req.body;
+    const [updateUserStatus] = await connection.execute(
+        "UPDATE product set status=? where id=?",
+        [user.status, user.id]
+    )
+    if(updateUserStatus.affectedRows == 0){
+    throw new ApiError(400, "Product ID not found")
+   }
+     return res.status(200)
+   .json(
+    new ApiResponse(200, updateUserStatus, "Product Deleted Successfully")
+   )
+})
+export {addProductDeatils, getProductDetails, getByCategoryId, getById, updateProductDetails, deleteProduct, updateStatus}
---
+++ Server/src/routes/product.router.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import authenticateToken from "../middlewares/auth.middleware.js";
+import checkRole from "../middlewares/checkrole.middleware.js";
+import { addProductDeatils, deleteProduct, getByCategoryId, getById, getProductDetails, updateProductDetails, updateStatus } from "../controllers/product.controller.js";
+
+const productRouter = Router()
+
+productRouter.post('/add-product-details', authenticateToken, checkRole, addProductDeatils)
+productRouter.get('/get-product-details', authenticateToken, getProductDetails)
+productRouter.get('/get-by-categoryid/:id', authenticateToken, getByCategoryId)
+productRouter.get('/get-by-id/:id', authenticateToken, getById)
+productRouter.patch('/update-product-details', authenticateToken, checkRole, updateProductDetails)
+productRouter.delete('/delete-product/:id', authenticateToken, checkRole, deleteProduct)
+productRouter.patch('/update-status', authenticateToken, checkRole, updateStatus)
+
+
+export default productRouter
